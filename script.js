b36b4c34bfccb9bdb4909cb8e951ec48b34f3d4f
Bryan Lilburn <bryanlilburn@gmail.com>
2020-09-24 21:53:23 -0700

    resolved merge conflicts

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
-// let ambeeAPI = "RjbEzFhLiK6G13X0NrxD94kbFuy94zx21vvV3JSe"
-// let queryURL2 = https://docs.ambeedata.com/#/By%20Postal%20Code/findDataByPostalCode
+//source: https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API
 
-let newsAPI = "e51ca59b75a04ae495dc5ab37f9b5ce0"
-// need to establish what criteria to search for //
-let queryURL3 = "https://newsapi.org/v2/everything?q=" + search-criteria + "&apiKey="  + newsAPI
-
-
-
-$("button").on("click", function (event) {
-    event.preventDefault();
-    let cityWeather = $("input").val().trim()
-
-    let weatherAPI = "fe4a853726c79035bac73e26c523869e";
-    let queryURL1 = "https://api.openweathermap.org/data/2.5/weather?q=" + cityWeather + "&appid=" + weatherAPI
-
+const callComplete = (userLocation) => {
+    console.log(userLocation);
+    console.log(userLocation.coords.latitude);
+    console.log(userLocation.coords.longitude);
+    let latitude = userLocation.coords.latitude;
+    let longitude = userLocation.coords.longitude;
+    
+   
+    let queryURL = "https://nominatim.openstreetmap.org/reverse?format=geojson&lat=" + latitude + "&lon=" + longitude;
     $.ajax({
-        url: queryURL1,
-        method: "GET"
-    })
+        url: queryURL,
+        method: "GET",
+      //  lat: userLocation.coords.latitude,
+      //  lng: userLocation.coords.longitude,
+    }).then(function (userLocation) {
+        console.log(userLocation);
 
-        .then(function (response) {
-            console.log(response);
-            $("#cityName").text(response.name)
+        let zipCode = userLocation.features[0].properties.address.postcode;
+        let userState = userLocation.features[0].properties.address.state;
+        let countryCode = userLocation.features[0].properties.address.country_code;
 
-            let tempF = (response.main.temp - 273.15) * 1.80 + 32
-            $("#temp").text("Temperature: " + Math.floor(tempF) + " °F")
-
-            $("#humidity").text("Humidity: " + response.main.humidity + "%")
-
-            $("#wind").text("Wind speed: " + response.wind.speed + " MPH")
-        });
+        console.log(zipCode);
+        console.log(userState);
+        console.log(countryCode);
+    });
+};
+//log geolocation call error
+const callProblem = (problem) => {
+    console.error(problem)
+};
+navigator.geolocation.getCurrentPosition(callComplete, callProblem); {
+//more accurate, larger power consumption
+enableHighAccuracy: true
+//time out geolocation call after 15 seconds
+timeout: 15000
+}
